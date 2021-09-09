399759741d57aef8c727129a7708df554d4bf54e
Dilshan <dilshan.mudali@gmail.com>
2021-09-09 15:04:19 -0400

    ej

--- phase-1/07-async-js-and-fetch/index.js
+++ phase-1/07-async-js-and-fetch/index.js
@@ -1,38 +1,24 @@
-const pokemons = [
-  {
-    id: 1,
-    name: "bulbasaur",
-    img: "https://static.wikia.nocookie.net/kingdom-keymasters-database/images/2/27/001Bulbasaur_XY_anime.png/revision/latest?cb=20160927122032",
-    likes: 4,
-  },
-  {
-    id: 2,
-    name: "ivysaur",
-    img: "https://static.wikia.nocookie.net/kingdom-keymasters-database/images/c/ca/002Ivysaur_XY_anime_2.png/revision/latest?cb=20161017212021",
-    likes: 21,
-  },
-  {
-    id: 3,
-    name: "venusaur",
-    img: "https://images.saymedia-content.com/.image/t_share/MTc2MjYwODQ5NTk2NTcyODYy/pokemon-venusaur-nicknames.png",
-    likes: 7,
-  },
-  {
-    id: 4,
-    name: "charmander",
-    img: "https://pixy.org/download/1207107/",
-    likes: 20,
-  },
-  {
-    id: 5,
-    name: "charmeleon",
-    img: "https://static.wikia.nocookie.net/pokemon-quest-adventures/images/a/a4/005Charmeleon_XY_anime_2.png/revision/latest?cb=20170313041459",
-    likes: 11,
-  },
-];
+
 
 const pokeContainer = document.getElementById("poke-container");
 const pokeForm = document.getElementById("poke-form");
+// const url = "http://localhost:3000/pokemons"
+
+function getPokemon(){
+  fetch('http://localhost:3000/pokemons')
+  .then(function(response){
+    return response.json()
+  })
+  .then(function(data){
+    data.forEach(function(pokemon){
+      renderPokemon(pokemon)
+    })
+  })
+}
+
+
+
+
 
 function renderPokemon(pokemon) {
   const pokeCard = document.createElement("div");
@@ -92,9 +78,7 @@ function deletePoke(card) {
 }
 
 function init() {
-  pokemons.forEach(function (pokemon) {
-    renderPokemon(pokemon);
-  });
+  getPokemon()
   pokeForm.addEventListener("submit", createPokemon);
 }
 
